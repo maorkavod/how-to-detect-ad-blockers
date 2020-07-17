8871b5ed57d74c0a82e0affe16c10f0c5043de31
maorkavod <maor.kavod@gmail.com>
2020-07-17 08:44:00 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,32 +1,31 @@
-document.addEventListener('DOMContentLoaded', init, false);
+async function checkForAdBlocker() {
+  let Blocked;
 
-function init(){
-  adsBlocked(function(blocked){
-    if(blocked){
-      document.getElementById('result').innerHTML = 'ads are blocked';
-    } else {
-      document.getElementById('result').innerHTML = 'ads are not blocked';
+  async function Request() {
+    try {
+      return fetch(
+        new Request(
+"https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js", {
+          method: 'HEAD',
+          mode: 'no-cors'
+        }))
+        .then(function(response) {
+          // There is no AdBlocker
+          Blocked = false;
+          return Blocked;
+        }).catch(function(e) {
+          // Failed, Because of an AdBlocker
+          Blocked = true;
+          return Blocked;
+        });
+    } catch (error) {
+      console.log(error);
+      Blocked = true;
+      return Blocked;
     }
-  })
+  }
+
+  return Blocked !== undefined ? Blocked : await Request();
 }
 
-function adsBlocked(callback){
-  var testURL = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js'
-
-  var myInit = {
-    method: 'HEAD',
-    mode: 'no-cors'
-  };
-
-  var myRequest = new Request(testURL, myInit);
-
-  fetch(myRequest).then(function(response) {
-    return response;
-  }).then(function(response) {
-    console.log(response);
-    callback(false)
-  }).catch(function(e){
-    console.log(e)
-    callback(true)
-  });
-}
+const usingBlocker = await checkForAdBlocker();
